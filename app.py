d878821b6405303798231ee80a75e5a2be7143e1
vvsviana86-cmyk <vvsviana86@gmail.com>
2026-06-13 00:47:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ with st.form("novo_gasto_form"):
         "Moradia", "Alimentação", "Transporte", "Assinaturas", 
         "Cuidados Pessoais", "Diversos", "Graduação", 
         "Vestimenta", "Tecnologia", "Móveis", "Eletrodomésticos", 
-        "Lazer", "Saúde", "Cigarro", "Reserva de Emergência"
+        "Lazer", "Saúde", "Cigarro", "Reserva de Emergência", "Comer Fora na Semana"
     ])
     
     tipo = st.radio("Tipo", ["Fixo", "Variável"])
